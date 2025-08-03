dd74bb5dcb983544022ed3db53ae085adf81e777
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-03 19:24:16 +0100

    fix: updated the main default widget to my app widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:todo_flutter_appwrite/main.dart';
+import 'package:todo_flutter_appwrite/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
