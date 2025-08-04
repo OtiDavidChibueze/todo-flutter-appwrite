23cec9c80b8ed022e7a76969de3d3ba311490cf9
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-04 23:51:02 +0100

    feat(main) init dotenv & setUpLocator

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:todo_flutter_appwrite/app.dart';
+import 'package:todo_flutter_appwrite/core/di/locator.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load();
+
+  setUpLocator();
+
   runApp(
     ScreenUtilInit(
       designSize: const Size(375, 812),
