c79c21c5316d611fe7afdfe6437c93b936249662
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-04 22:39:24 +0100

    feat: add login route

--- lib/core/router/routes.dart
+++ lib/core/router/routes.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:todo_flutter_appwrite/features/auth/presentation/pages/login.dart';
 import 'package:todo_flutter_appwrite/features/auth/presentation/pages/register.dart';
 import 'package:todo_flutter_appwrite/features/splash/presentation/pages/splash_page.dart';
 
@@ -15,6 +16,12 @@ final class AppRoutes {
         path: '/register',
         name: RegisterPage.routeName,
         builder: (context, state) => RegisterPage(),
+      ),
+
+      GoRoute(
+        path: '/login',
+        name: LoginPage.routeName,
+        builder: (context, state) => LoginPage(),
       ),
     ],
   );
