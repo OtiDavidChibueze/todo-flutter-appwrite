f23062b5245c88429a976cb857441cdcb3286edd
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-04 22:28:02 +0100

    feat: add register page strings

--- lib/core/constants/app_string.dart
+++ lib/core/constants/app_string.dart
@@ -2,4 +2,10 @@ class AppString {
   static const String failure = 'Sorry, An unexpected error occured!';
   static const String required = 'Field is required!';
   static const String fullname = 'Full Name';
+  static const String lastname = 'Last Name';
+  static const String email = 'Email';
+  static const String emailValidation = 'Input a valid email!';
+  static const String password = 'Password';
+  static const String register = 'Register';
+  static const String login = 'Login';
 }
