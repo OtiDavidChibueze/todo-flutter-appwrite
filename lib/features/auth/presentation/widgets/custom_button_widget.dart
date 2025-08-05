73f19f12ab8b24c13bc890317097ff90b396e8c9
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-05 03:43:45 +0100

    feat(customWidget): add custom widget

---
+++ lib/features/auth/presentation/widgets/custom_button_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:todo_flutter_appwrite/core/common/theme/app_color.dart';
+
+import 'package:todo_flutter_appwrite/core/utils/size_utils.dart';
+
+class CustomButtonWidget extends StatelessWidget {
+  final double width;
+  final double height;
+  final Color backgroundColor;
+  final double radius;
+  final String btnText;
+  final double fontSize;
+  final Color textColor;
+  final Function() onPressed;
+
+  const CustomButtonWidget({
+    super.key,
+    this.width = 100,
+    this.height = 6,
+    this.backgroundColor = AppColor.appColor,
+    this.radius = 10,
+    required this.btnText,
+    this.fontSize = 15,
+    this.textColor = AppColor.whiteColor,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ButtonStyle(
+        elevation: WidgetStateProperty.all(0),
+
+        backgroundColor: WidgetStateProperty.all(backgroundColor),
+        fixedSize: WidgetStateProperty.all(Size(sw(width), sh(height))),
+        shape: WidgetStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(sr(radius)),
+          ),
+        ),
+      ),
+      child: Text(
+        btnText,
+        style: TextStyle(fontSize: sp(fontSize), color: textColor),
+      ),
+    );
+  }
+}
---
+++ lib/features/auth/presentation/widgets/custom_textfield_widget.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:todo_flutter_appwrite/core/common/theme/app_color.dart';
+
+class CustomTextfieldWidget extends StatelessWidget {
+  final TextEditingController? controller;
+  final bool obscureText;
+  final TextInputType? keyboardType;
+  final String? Function(String?)? validator;
+  final String hintText;
+  final Widget? suffix;
+
+  const CustomTextfieldWidget({
+    super.key,
+    this.controller,
+    this.obscureText = false,
+    this.keyboardType,
+    this.validator,
+    required this.hintText,
+    this.suffix,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: validator,
+      decoration: InputDecoration(
+        hintText: hintText,
+        border: InputBorder.none,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: AppColor.borderColor, width: 1),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: AppColor.appColor, width: 1),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: AppColor.errorColor, width: 1),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: AppColor.errorColor, width: 1),
+        ),
+        suffix: suffix,
+      ),
+    );
+  }
+}
