60c6690be3f68931099e0bc31edb1d9aa70d7195
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-05 03:25:43 +0100

    refactor(email V..): add a null or empty check, feat: add isEmpty string & password val...

--- lib/core/utils/validation/validators.dart
+++ lib/core/utils/validation/validators.dart
@@ -1,5 +1,24 @@
+import 'package:todo_flutter_appwrite/core/constants/app_string.dart';
+
 class Validations {
-  static var emailValidation = RegExp(
-    r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
-  );
+  static String? isEmpty(String? value) {
+    if (value == null || value.isEmpty) return AppString.required;
+    return null;
+  }
+
+  static String? email(String? value) {
+    if (value == null || value.isEmpty) return AppString.required;
+
+    final pattern = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+    if (!pattern.hasMatch(value)) return AppString.emailValidation;
+    return null;
+  }
+
+  static String? password(String? value) {
+    if (value == null || value.isEmpty) return AppString.required;
+
+    if (value.length <= 5) return AppString.passwordTooShort;
+
+    return null;
+  }
 }
