4640a1eb15feb61edcd1f755ca44dd19030da598
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-06 01:31:37 +0100

    fix(main): init hive flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hive/hive.dart';
 
 import 'app.dart';
 import 'core/di/locator.dart';
@@ -11,6 +12,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load();
+
+  await Hive.initFlutter();
 
   setUpLocator();
 
