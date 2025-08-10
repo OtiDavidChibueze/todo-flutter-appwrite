57c13dec52250f7e99f57227a372ebfcb0a1f41b
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-08-10 18:26:37 +0100

    style(font): add global fontstyle

--- lib/core/common/theme/app_theme.dart
+++ lib/core/common/theme/app_theme.dart
@@ -2,16 +2,14 @@ import 'app_color.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static final dartThemeMode = ThemeData.dark(useMaterial3: true).copyWith(
+  static final dartThemeMode = ThemeData(
+    brightness: Brightness.dark,
+    useMaterial3: true,
+    fontFamily: 'Delius',
     scaffoldBackgroundColor: AppColor.backgroundColor,
     appBarTheme: AppBarTheme(backgroundColor: AppColor.appbarColor),
     progressIndicatorTheme: ProgressIndicatorThemeData(
       color: AppColor.appColor,
     ),
-  );
-
-  static final lightThemeMode = ThemeData.light(useMaterial3: true).copyWith(
-    scaffoldBackgroundColor: AppColor.inverseBackgroundColor,
-    appBarTheme: AppBarTheme(color: AppColor.appbarInverse),
   );
 }
